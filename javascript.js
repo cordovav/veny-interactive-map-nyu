f55fe8a641161a0bd9b06b254b2f2362ce0ea46e
Veny Cordova <venycordova@gmail.com>
2022-10-21 21:13:32 -0400

    added foursquare to code

--- javascript.js
+++ javascript.js
@@ -1,67 +1,78 @@
 //we need a map to get the user location
-const myMap = {
-        coordinates: [],
-        business: [],
-        map: {},
-        markers: {},
-
-//leafletmap
-    createMap(){
-        console.log(this.coordinates)
-        myMap.map = L.map('map',{
-            center: this.coordinates,
-            zoom: 11,
-        });
     
-//we need to map on Leaflet Map
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        minZoom: '15',
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(this.map);
 
-        //geolocation marker
-        const marker = L.marker(this.coordinates)
-        marker.addTo(this.map).bindPopup('<p>You Are Here</p>').openPopup();
-    }
-}
+
+
 // Get the user's coordinates:                                                              
 async function getCoords(){
-    const pos = await new Promise((resolve, reject) => {
+    pos = await new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+    });
     return [pos.coords.latitude, pos.coords.longitude]
 }
 
 //created a load up window
 // On load, build ads:                                                             
 window.onload = async () => {
-    myMap.coordinates = await getCoords()
-    console.log(myMap.coordinates)
-    myMap.createMap();
-}
+    myMap = L.map('map',{
+        center: await getCoords(),
+        zoom:12,
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        minZoom: '15',
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(myMap);
+
+        //geolocation marker
+        const marker = L.marker([pos.coords.latitude, pos.coords.longitude]);
+        marker.addTo(myMap).bindPopup('<p>You Are Here</p>').openPopup();
+};
+
 
 //Create an event to ask for the user location
 document.querySelector('#submit').addEventListener('click', async (event) =>{
     event.preventDefault()
-    let business = document.querySelector('#busniess').value
-    console.log(busniess)
-})
+    let business = document.querySelector('#business').value;
+    let latlong = await getCoords()
+    let lat = latlong[0]
+    let long = latlong[1]
+    let data = await getFourSquare(business, lat, long);
+    let response = data.json();
+    console.log(response)
+    //myMap.businesses = processBusinesses(response);
+    //myMap.addMarkers();
 
-//Allow the user to select a business type from Foursquare API
-const options = {method: 'GET', headers: {accept: 'application/json', Authorization: 'me'}};
+});
 
-fetch('https://api.foursquare.com/v3/places/search', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-async function getFoursquare(business){
-    const options = {
-        method: 'GET',
-        headers: {
-        
-        Accept:'Content-Type': 'application/json',
-        Authorization: "",
-    
+async function getFourSquare(business, lat, long){
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: ''
     }
+    };
+    let params = new URLSearchParams({
+        query: business,
+        ll: `${lat},${long}`,
+        limit: 5
+    })
+    return await fetch(`https://api.foursquare.com/v3/places/search?&${params}`, options)
+    
 }
+/*
+function processBusinesses(data){
+    let businesses = data.map((element) =>{
+        let location ={
+            name: element.name,
+            lat: element.geocodes.main.latitude,
+            long: element.geocodes.main.longitude
+        };
+        return location;
+    })
+    return businesses;
+}
+*/
+
